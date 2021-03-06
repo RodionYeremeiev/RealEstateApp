39bb8e3c6b5a3b0fbac36dcbd53e2434c4da1bb9
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-03-06 15:52:25 +0200

    ApartmentRaterTest added test for average rate calulation with correct & empty List

--- src/test/java/com/realestateapp/ApartmentRaterTest.java
+++ src/test/java/com/realestateapp/ApartmentRaterTest.java
@@ -1,12 +1,16 @@
 package com.realestateapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApartmentRaterTest {
 
@@ -15,7 +19,7 @@ class ApartmentRaterTest {
             resources = "/ApartmentParameters.csv",
             numLinesToSkip = 1
     )
-    void should_ReturnCorrectRating_When_CorrectApartment(double area, double price, int rating){
+    void should_ReturnCorrectRating_When_CorrectApartment(double area, double price, int rating) {
         // given
         Apartment apartment = new Apartment(area, BigDecimal.valueOf(price));
         int expected = rating;
@@ -29,7 +33,7 @@ class ApartmentRaterTest {
     }
 
     @Test
-    void should_ReturnErrorValue_When_IncorrectApartment(){
+    void should_ReturnErrorValue_When_IncorrectApartment() {
         // given
         Apartment apartment = new Apartment(0, BigDecimal.valueOf(35000.0));
         int expected = -1;
@@ -41,24 +45,35 @@ class ApartmentRaterTest {
         assertEquals(expected, actual);
 
     }
+
     @Test
-    void should_CalculateAverageRating_When_CorrectApartmentList(){
+    void should_CalculateAverageRating_When_CorrectApartmentList(double area, double price) {
         // given
+        List<Apartment> apartments = new ArrayList<>();
+        apartments.add(new Apartment(50.0, BigDecimal.valueOf(35000.0)));
+        apartments.add(new Apartment(100.0, BigDecimal.valueOf(750000.0)));
+        apartments.add(new Apartment(60.0, BigDecimal.valueOf(600000.0)));
+        double expected = 1;
 
         // when
+        double actual = ApartmentRater.calculateAverageRating(apartments);
 
         // then
+        assertEquals(expected, actual);
+
 
     }
 
     @Test
-    void should_ThrowExceptionInCalculateAverageRating_When_EmptyApartmentList(){
+    void should_ThrowExceptionInCalculateAverageRating_When_EmptyApartmentList() {
         // given
+        List<Apartment> apartments = new ArrayList<>();
 
         // when
+        Executable executable = () -> ApartmentRater.calculateAverageRating(apartments);
 
         // then
-
+        assertThrows(RuntimeException.class, executable);
     }
 
 }
